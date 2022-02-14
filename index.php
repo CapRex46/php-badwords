2b9d5f48234e64af188fa82765909307ff9f8deb
agres <ale.agresta46@gmail.com>
2022-02-14 16:05:32 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $txt = "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquam iure possimus quod ut perspiciatis. At eligendi repellendus eveniet, aperiam alias, doloribus corporis, nihil sunt similique quidem magnam doloremque ipsa voluptatibus!";
+        $txtlen = strlen($txt);
+        $censura = $_GET["lorem"];
+        $censuratxt = str_replace($censura, "***", $txt);
+    ?> 
+    <h1><?php echo $txt ?></h1>
+    <div>
+        Lunghezza testo: <?php echo $txtlen ?> 
+    </div>
+    <h3>Testo con censura:</h3>
+    <p>
+        <?php echo $censuratxt ?>
+    </p>
+</body>
+</html>
